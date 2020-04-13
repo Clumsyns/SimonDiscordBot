2310552b251c41ba69887fb505a8dc89b7c2069d
unknown <clumsyns@outlook.com>
2020-04-12 22:11:56 -0500

    added ban embed

--- cmds/ban.js
+++ cmds/ban.js
@@ -10,7 +10,18 @@ module.exports = class ban {
   async run(client, message, args) {
     var mentioned = message.mentions.users.first();
     var member = message.guild.member(mentioned);
+
+    const banEmbed = new Discord.MessageEmbed()
+    .setColor('#220f99')
+    .setAuthor('SimonBot', client.user.displayAvatarURL())
+    .setDescription(`${mentioned} has been banned.`)
+    .setImage('https://media3.giphy.com/media/MEw0inp5gAlzO/source.gif')
+    .setTimestamp()
+    .setFooter('Made with <3 by Clumsy', client.user.displayAvatarURL());
+
+    message.channel.send(banEmbed);
+
     member.ban();
   }
-  
+
 }
